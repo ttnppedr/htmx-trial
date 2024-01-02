145aa2981697e38a1faabdfc32cae86ec13207c7
ttnppedr <priest0204@gmail.com>
2024-01-02 23:42:35 +0800

    Add form validation

--- server.js
+++ server.js
@@ -62,9 +62,7 @@ app.post('/search/api', async (req, res) => {
         return res.send('<tr></tr>');
     }
 
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users`
-    );
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const contacts = await response.json();
 
     const searchResults = contacts.filter((contact) => {
@@ -72,19 +70,79 @@ app.post('/search/api', async (req, res) => {
         const email = contact.email.toLowerCase();
 
         return name.includes(searchTerm) || email.includes(searchTerm);
-    })
+    });
 
     setTimeout(() => {
-        const searchResultsHTML = searchResults.map(
-            (contact) => `
+        const searchResultsHTML = searchResults
+            .map(
+                (contact) => `
                 <tr>
                     <td><div class="my-4 p-2">${contact.name}</div></td>
                     <td><div class="my-4 p-2">${contact.email}</div></td>
                 </tr>
-            `).join('');
+            `
+            )
+            .join('');
 
         res.send(searchResultsHTML);
     }, 1000);
+});
+
+app.post('/contact/email', (req, res) => {
+    const submittedEmail = req.body.email;
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    const isValid = {
+        message: 'That email is valid',
+        class: 'text-green-700',
+    };
+
+    const isInvalid = {
+        message: 'Please entar a valid email address',
+        class: 'text-red-700',
+    };
+
+    if (!emailRegex.test(submittedEmail)) {
+        return res.send(
+            `
+      <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email Address</label
+        >
+        <input
+          name="email"
+          hx-post="/contact/email"
+          type="email"
+          class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+          id="email"
+          required
+          value="${submittedEmail}"
+        />
+        <div class="${isInvalid.class}">${isInvalid.message}</div>
+      </div>
+        `
+        );
+    } else {
+        return res.send(
+            `
+      <div class="mb-4" hx-target="this" hx-swap="outerHTML">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email Address</label
+        >
+        <input
+          name="email"
+          hx-post="/contact/email"
+          type="email"
+          class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+          id="email"
+          required
+          value="${submittedEmail}"
+        />
+        <div class="${isValid.class}">${isValid.message}</div>
+      </div>
+        `
+        );
+    }
 });
 
 app.listen(3000, () => {
