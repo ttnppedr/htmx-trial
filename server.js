2474e16fde641e32cfd7a2585a53d3635f31ac65
ttnppedr <priest0204@gmail.com>
2024-01-02 17:42:44 +0800

    Use hx-post

--- server.js
+++ server.js
@@ -7,22 +7,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
-  setTimeout(async () => {
-    const limit = +req.query.limit || 10;
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
-    );
-    const users = await response.json();
+    setTimeout(async () => {
+        const limit = +req.query.limit || 10;
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+        );
+        const users = await response.json();
 
-    res.send(`
+        res.send(`
               <h1 class="text-2xl font-bold my-4">Users</h1>
               <ul>
                   ${users.map((user) => `<li>${user.name}</li>`).join('')}
               </ul>
           `);
-  }, 2000);
+    }, 2000);
+});
+
+app.post('/convert', (req, res) => {
+    setTimeout(() => {
+        const fahrenheit = parseFloat(req.body.fahrenheit);
+        const celsius = (fahrenheit - 32) * (5 / 9);
+
+        res.send(`
+          <p>
+            ${fahrenheit} degree Fahrenheit is equal to ${celsius.toFixed(
+            2
+        )} degree Celsius.
+          </p>
+        `);
+    }, 2000);
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+    console.log(`Server is running on port 3000`);
 });
