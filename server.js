7b5659f1e0ea71e1ae764d8e0682bbcdc6ed796d
ttnppedr <priest0204@gmail.com>
2024-01-02 17:02:13 +0800

    Add fake users data route

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Bob Williams' },
+    { id: 3, name: 'Shannon Jackson' },
+  ];
+
+  res.send(`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join('')}
+        </ul>
+    `);
+});
+
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 3000`);
 });
