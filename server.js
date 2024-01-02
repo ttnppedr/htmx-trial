474c2c6f3e84b8d0cfec54214afb28d78a3441a6
ttnppedr <priest0204@gmail.com>
2024-01-02 17:12:09 +0800

    Fetch users from api

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  const users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Bob Williams' },
-    { id: 3, name: 'Shannon Jackson' },
-  ];
+app.get('/users', async (req, res) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users'); 
+  const users = await response.json();
 
   res.send(`
         <h1 class="text-2xl font-bold my-4">Users</h1>
