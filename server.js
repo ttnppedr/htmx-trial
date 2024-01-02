fc998dae887be9dbd25ffd74595eb78512776a32
ttnppedr <priest0204@gmail.com>
2024-01-02 17:22:06 +0800

    Use hx-vals

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
-    setTimeout(async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const users = await response.json();
+  setTimeout(async () => {
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
 
-        res.send(`
+    res.send(`
               <h1 class="text-2xl font-bold my-4">Users</h1>
               <ul>
                   ${users.map((user) => `<li>${user.name}</li>`).join('')}
               </ul>
           `);
-    }, 2000);
+  }, 2000);
 });
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 3000`);
 });
